7a3bcfc7ecff2e34cd9455625c0887e4c9b69349
oohashiLaboratory <93508608+oohashiLaboratory@users.noreply.github.com>
2022-03-04 13:42:56 +0900

    Add files via upload

---
+++ Receive_upload/Receive_upload.ino
@@ -0,0 +1,141 @@
+//データ受信側（スレイブ）
+#include <M5StickC.h>
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include "Ambient.h"    //Ambient
+
+const char* ssid     = "test.lab";//WIFI名
+const char* password = "akatuki00";//wifiパスワード
+unsigned int channelId = 38698; // AmbientチャネルID
+const char* writeKey = "bcc550e6d9ce9eba"; // Ambientライトキー
+
+bool ledState = false;
+float vbat = 0;//本体電圧
+
+float Data[10];
+float env = 0;
+WiFiClient client;
+Ambient ambient;
+int RNo;//送信機番号
+void toggleLed()    //LEDの状態切り替え関数
+{
+    if (ledState)
+    {
+        ledState = false;   //LEDステータスをfalseに
+       digitalWrite(GPIO_NUM_10, LOW); //LEDON
+    }
+    else
+    {
+        ledState = true;    //LEDステータスをtrueに
+        digitalWrite(GPIO_NUM_10, HIGH); //LEDOFF
+    }
+}
+
+void onReceive(const uint8_t* mac_addr, const uint8_t* ReceiveData, int ReceiveData_len)//受信アドレス 受信データ（配列数） 受信データ数(配列数)
+{
+    M5.Lcd.fillScreen(BLACK);
+    m5.Lcd.setCursor(2,0);
+    char macStr[18];
+    snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+        mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);  //配列macStrにmac_addrのデータを格納
+    Serial.println();
+
+    M5.lcd.setCursor(0,0);
+    M5.Lcd.printf("Last Packet Recv from: %s\n", macStr);   //最後のパケット受信元
+    M5.Lcd.printf("Last Packet Recv data(%d): ", ReceiveData_len); //最後の受信データ数
+
+    if (ReceiveData[0] <= 9)//機器判別番号が10（所定数）以下
+    {
+        
+        RNo = ReceiveData[0];
+        if(ReceiveData[1]==0)
+        {
+            env = ((float)ReceiveData[2]+((float)ReceiveData[3]/100))*(-1);
+            
+        }
+        else
+        {
+            env = (float)ReceiveData[2]+((float)ReceiveData[3]*(float)0.01);
+            Data[RNo]=(float)env;
+        }
+     }
+    for (int i = 0; i < ReceiveData_len; i++)
+    {
+        M5.Lcd.print(ReceiveData[i]); //受信データの表示
+        M5.Lcd.print(" ");
+        if (ReceiveData[i] == 222)//現状data[1]までは他のデータのため2からに
+        {
+            toggleLed();//LED状態切り替え
+        }
+    }
+
+    m5.Lcd.setCursor(2,40);
+    M5.Lcd.print("Recv data: \n"); //受信データ
+    for (int i = 0; i < 10; i++)
+    {
+        M5.Lcd.print(Data[i]);
+        M5.Lcd.print(" ");
+    }
+
+}
+
+void setup()
+{
+    M5.begin();
+    Serial.begin(115200);
+    M5.Lcd.setRotation(1);                        //テキストサイズを1に
+    M5.Lcd.setTextColor(YELLOW,BLACK);            //テキストの色を黄色に変更
+    pinMode(GPIO_NUM_10, OUTPUT);
+    M5.Lcd.println(WiFi.macAddress()); // このアドレスを送信側へ登録します
+
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    if (esp_now_init() == ESP_OK)
+    {
+        m5.Lcd.println("ESP-Now Init Success");
+    }
+    esp_now_register_recv_cb(onReceive);//esp_now_register_recv_cb(受信時に実行する関数);
+    digitalWrite(GPIO_NUM_10, HIGH); //LEDOFF
+}
+
+void ambient_access(void)       //アンビエントアクセス関数
+{
+    vbat = M5.Axp.GetBatVoltage();//バッテリ電圧取得
+    ambient.begin(channelId, writeKey, &client); //チャネルIDとライトキーを指定してAmbientの初期化
+
+    //Ambientに送信するデータをセット
+    for(int i=1;i<7;i++)
+    {
+        ambient.set(i, Data[i-1]);
+    }
+
+    ambient.send();                   //ambientにデータを送信
+}
+
+void wifi_conect(void)  //wifi接続関数
+{
+    WiFi.begin(ssid, password);           //  Wi-Fi APに接続
+
+    while (WiFi.status() != WL_CONNECTED) //  Wi-Fi AP接続待ち
+    {
+       delay(500);
+    }
+
+    Serial.print("WiFi connected\r\nIP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+
+void loop()
+{
+    M5.update();        //ボタン情報の更新
+    if ( M5.BtnA.wasPressed() )//LCD下部のボタンが押されたとき
+    {
+      wifi_conect();//wifiに接続
+      ambient_access();//ambientにデータ送信
+      WiFi.disconnect();  //wifi切断
+      delay(100);
+    }
+    delay(1);//0.1秒待機
+}
---
+++ sendEnvData/sendEnvData.ino
@@ -0,0 +1,132 @@
+//送信側（マスター）
+#include <M5StickC.h>
+#include <esp_now.h>
+
+#include <WiFi.h>
+#include <Wire.h>
+
+#include "Adafruit_Sensor.h"
+
+#include "SHT3X.h"   //M5Stack用環境センサユニット ver.2
+#include <Adafruit_BMP280.h>
+
+uint8_t ReceiveAddress[] = { 0x94, 0xB9, 0x7E, 0x92, 0xE7, 0xCC }; // 受信機のMACアドレスに書き換える macアドレスはM5.Lcd.println(WiFi.macAddress())で確認する0x94, 0xB9, 0x7E, 0x92,までは固定
+//uint8_t ReceiveAddress2[] = { 0x94, 0xB9, 0x7E, 0x92, 0xB2, 0x48 };
+
+float tmp = 0.0;      //温度変数
+float hum = 0.0;      //湿度変数
+int i=0;
+SHT3X sht30;        //M5Stack用環境センサユニット ver.2
+Adafruit_BMP280 bme;
+
+
+void onSend(const uint8_t* mac_addr, esp_now_send_status_t status)//送信が完了
+{
+    char macStr[18];
+    snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+        mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+
+    //M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(5, 10);
+    M5.Lcd.println(macStr); //受信機のmacアドレスを表示
+    M5.Lcd.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Failed");//送信成功or失敗を表示
+}
+
+
+void setup()
+{
+    M5.begin();
+    Wire.begin();               // I2Cを初期化する
+    M5.Lcd.setRotation(1);  //LCDを横向きに
+    M5.Lcd.fillScreen(BLACK);//LCD黒で塗りつぶし
+    M5.Lcd.setRotation(1);                        //テキストサイズを1に
+    M5.Lcd.setTextColor(YELLOW,BLACK);            //テキストの色を黄色に変更
+
+    WiFi.mode(WIFI_STA);                          //wifiモードをSTA（子機）モードに変更
+    WiFi.disconnect();                            //WIFIから切断
+
+    if (esp_now_init() == ESP_OK)                 //ESPNOWの初期化成功時
+    {
+        Serial.println("ESPNow Init Success");  //ESPNOW初期化成功
+    }
+
+    esp_now_peer_info_t peerInfo;
+    memcpy(peerInfo.peer_addr, ReceiveAddress, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    if (esp_now_add_peer(&peerInfo) != ESP_OK)  //ペア追加が失敗したとき
+    {
+        Serial.println("Failed to add peer");
+        return;
+    }
+
+
+    esp_now_register_send_cb(onSend);   //送信が完了したときのイベント
+}
+
+void loop()
+{
+
+    uint8_t data[5] = { 0 ,0, 0,0 , 222 }; //送信データ
+    //float data[3] = { 0 ,0, ,222 }; //送信データ
+
+    M5.update();        //ボタン情報の更新
+
+    if ( M5.BtnA.wasPressed() )//LCD下部のボタンが押されたとき
+    {
+        if(sht30.get() == 0)
+        {
+            tmp = sht30.cTemp;              //温度取り込み
+        }
+        float decimal;
+        if(tmp<0)//tmpがマイナスの時
+        {
+            data[1] = 0;
+        }
+        else
+        {
+            data[1] = 1;
+        }
+
+        data[2] = tmp; //送信データ
+        data[3] = (tmp - (int)tmp)*100;//小数点以下を入れる処理tmp//小数点以下を送る
+        esp_err_t result = esp_now_send(ReceiveAddress, data, sizeof(data));//データ送信(受信側のアドレス、符号、データ、データ（小数点以下）、データサイズ),送信ステータスの獲得
+
+        Serial.print("Send Status: ");//送信状態の表示
+        switch (result)
+        {
+        case ESP_OK:
+            M5.Lcd.println("Success");              //成功
+            break;
+        case ESP_ERR_ESPNOW_NOT_INIT:
+            M5.Lcd.println("ESPNOW not Init.");     //ESPNOWが初期化されていない
+            break;
+        case ESP_ERR_ESPNOW_ARG:
+            M5.Lcd.println("Invalid Argument");     //無効な引数
+            break;
+        case ESP_ERR_ESPNOW_INTERNAL:
+            M5.Lcd.println("Internal Error");       //内部エラー
+            break;
+        case ESP_ERR_ESPNOW_NO_MEM:
+            M5.Lcd.println("ESP_ERR_ESPNOW_NO_MEM");//メモリー不足
+            break;
+        case ESP_ERR_ESPNOW_NOT_FOUND:
+            M5.Lcd.println("Peer not found.");      //ペアが見つからない
+            break;
+
+        default:
+            M5.Lcd.println("Not sure what happened");//不明
+            break;
+        }
+      M5.Lcd.println(tmp);
+    for (int i = 0; i < 6; i++)
+    {
+        M5.Lcd.print(data[i]);
+        M5.Lcd.print(" ");
+    }
+    }
+
+
+    delay(1);
+}
